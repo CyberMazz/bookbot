e75fd999b29a736dd5d52b108810b31b1469c444
CyberMazz <mahzenbabekir@gmail.com>
2026-03-03 17:02:40 -0500

    created count_letters in stats.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import count_words, count_letters
+
 def get_book_text(filepath):
 	with open(filepath) as f:
 		file_contents = f.read()
@@ -6,7 +8,9 @@ def get_book_text(filepath):
 def main():
 	contents = get_book_text("books/frankenstein.txt")
 	num_words = count_words(contents)
+	char_counts = count_letters(contents)
 	print(f"Found {num_words} total words")
+	print(f"The letter counts are: {char_counts}")
 
 main()
 
--- stats.py
+++ stats.py
@@ -2,3 +2,12 @@ def count_words(text):
 	words = text.split()
 	num_words = len(words)
 	return num_words
+
+def count_letters(text):
+    counts = {}
+    for char in text.lower():
+        if char in counts:
+            counts[char] += 1
+        else:
+            counts[char] = 1  # first occurrence
+    return counts
