8642df0b9d56c139652a224fa960ab2348552ed7
CyberMazz <mahzenbabekir@gmail.com>
2026-03-03 16:36:41 -0500

    commit

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ def get_book_text(filepath):
 		file_contents = f.read()
 	return file_contents
 
-def count_words(text):
-	words = text.split()
-	num_words = len(words)
-	return num_words
-
 def main():
 	contents = get_book_text("books/frankenstein.txt")
 	num_words = count_words(contents)
